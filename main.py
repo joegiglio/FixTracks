e7da44a8fe50b534731ac3b49d0d6ffb464c6109
joeg816 <jgiglio3@optonline.net>
2022-02-24 21:36:48 -0500

    Basic concept works!

--- main.py
+++ main.py
@@ -7,19 +7,30 @@ from mutagen.flac import FLAC
 # xdirectory = os.path.join(os.sep, 'u:', os.sep, 'music', os.sep, 'Ripped', os.sep, '4 Non Blondes')
 #print(directory)
 
-directory = Path("U:/music/Ripped/4 Non Blondes/Bigger, Better, Faster, More!")
-file = directory / "01 Train.flac"
-#file = directory / "06 spaceman.flac"
+directory = Path("U:\music\Ripped\EZO\Fire Fire")
+bad_filename = "01 Love Junkie.flac" 
+bad_file = directory / bad_filename
 
-#f = open(file)
-# print(f.read())
+audio1 = FLAC(bad_file)
+print(audio1)
 
-audio = FLAC(file)
-print(audio)
+print("---")
 
-audio["album"] = u"JOE1-album"
-audio["artist"] = u"JOE-artist"
-audio.pprint()
-audio.save()
+good_file = file = directory / "02 Night Crawler.flac"
+audio2 = FLAC(good_file)
+print(audio2)
 
-print(audio)
+print("fixing")
+
+audio1["artist"] = audio2["artist"]
+audio1["albumartist"] = audio2["albumartist"]
+audio1["album"] = audio2["album"]
+audio1["title"] = bad_filename[3:-5]
+
+audio1.pprint()
+audio1.save()
+
+# audio["album"] = u"JOE1-album"
+# audio["artist"] = u"JOE-artist"
+# audio.pprint()
+# audio.save()
